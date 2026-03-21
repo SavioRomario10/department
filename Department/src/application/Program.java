168b4087f24b90bf361e87964fe01c0eb78831f7
Sávio Romário <savioromario10@gmail.com>
2026-03-21 15:02:23 -0300

    Seller and Department

---
+++ Department/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
+public class Program {
+
+  public static void main(String[] args) {
+    Department department = new Department(1, "books");
+    Seller seller = new Seller(1, "Maria", "4b6hj@example.com", new Date(), 3000.0, department);
+    System.out.println(seller);
+  }
+}
