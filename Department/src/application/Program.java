65e7870754e4d3a4968169658bbe1e806f4afdb0
Sávio Romário <savioromario10@gmail.com>
2026-03-21 15:07:52 -0300

    SellerDao and DepartmentDao

--- Department/src/application/Program.java
+++ Department/src/application/Program.java
@@ -8,8 +8,10 @@ import java.util.Date;
 public class Program {
 
   public static void main(String[] args) {
+    
     Department department = new Department(1, "books");
     Seller seller = new Seller(1, "Maria", "4b6hj@example.com", new Date(), 3000.0, department);
+
     System.out.println(seller);
   }
 }
